e2ff3785fc85279c1050eb8b7a4dd90cebcc6550
猪野麻巳子 <p@iinomamiko-no-MacBook-Pro.local>
2018-06-20 09:25:51 +0900

    Search reRelatedWords

--- Step0615.java
+++ Step0615.java
@@ -5,20 +5,21 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.lang.Integer;
 import java.lang.System;
+import java.util.Collections;
 
 public class Step0615{
     public static void main(String[] args){
         Scanner scanner =new Scanner(System.in);
-        System.out.println("類似性を調べたい単語を入力してください(単語1):");
-        String inputWord1= scanner.next();
-        System.out.println("類似性を調べたい単語を入力してください(単語2): ");
-        String inputWord2=scanner.next();
+        System.out.println("調べたい単語を入力してください:");
+        String inputWord= scanner.next();
+        
 
-        int inputWord1Index = wikiWordsSearch(inputWord1);
-        ArrayList<String> relatedWords1=relatedWordsSearch(inputWord1Index);
+        int inputWordIndex = wikiWordsSearch(inputWord);
+        ArrayList<Integer> relatedWordsIndex=relatedWordsSearch(inputWordIndex);
+        ArrayList<Integer> reRelatedWords=reRelatedWordsSearch(relatedWordsIndex);
+        double percent=reRelatedPerRelated(relatedWordsIndex,reRelatedWords);
+        System.out.println(percent);
 
-        int inputWord2Index = wikiWordsSearch(inputWord2);
-        ArrayList<String> relatedWords2=relatedWordsSearch(inputWord2Index);
 
         //double relationship = evaluateRelationship(relatedWords1,relatedWords2);
         //System.out.println("--------------------------------------------------");
@@ -61,15 +62,16 @@ public class Step0615{
         }
     }
      
-    public static ArrayList<String> relatedWordsSearch(int inputWordIndex){
+    public static ArrayList<Integer> relatedWordsSearch(int inputWordIndex){
         if(inputWordIndex==-1){
-            ArrayList<String> nullList = new ArrayList<String>(1);
+            ArrayList<Integer> nullList = new ArrayList<Integer>(1);
             return nullList;
         }else{
             int wordIndexs[] = new int[60000000];
             int wikiLinks[] = new int[60000000];
             ArrayList<String> relatedWords = new ArrayList<String>(150000);
-            String wikiWords[] = readWikiWords();
+            ArrayList<Integer> relatedWordsIndex = new ArrayList<Integer>(150000);
+            String[] wikiWords = readWikiWords();
             try{
                 Scanner scanner = new Scanner(new File("links.txt"));
                 System.out.println("open File, 'links.txt'");
@@ -92,73 +94,83 @@ public class Step0615{
             }
             System.out.println("Finish reading File, 'links.txt'");
             int paramForWordIndexs = 0;
-            int paramForWikiLinks=0;
+            int paramForRelatedWordsIndex=0;
             while (paramForWordIndexs < wordIndexs.length) {
                 if (wordIndexs[paramForWordIndexs] > inputWordIndex) {
                     break;
                 } else if (wordIndexs[paramForWordIndexs] == inputWordIndex) {
                     relatedWords.add(Arrays.asList(wikiWords).get(wikiLinks[paramForWordIndexs]));
+                    relatedWordsIndex.add(wikiLinks[paramForWordIndexs]);
+                    paramForRelatedWordsIndex++;
                 }
                 paramForWordIndexs++;
             }
             System.out.println("--------------------------------------------------");
-            System.out.println(Arrays.asList(wikiWords).get(inputWordIndex)+"のサイトから飛べるリンクの一覧を表示します");
+            System.out.println(Arrays.asList(wikiWords).get(inputWordIndex) + "のサイトから飛べるリンクの一覧を表示します");
             System.out.println(Arrays.asList(relatedWords));
-            return relatedWords;
+            Collections.sort(relatedWordsIndex);
+            return relatedWordsIndex;
+            //ここまでおっけい
         }
     
     }
-    
-    //public static double evaluateRelationship(ArrayList<String> relatedWords1, ArrayList<String> relatedWords2){
-        //int paramForRelatedWords = 0;
-        //int matchedNumber = 0;
-        //String matchedWords[] = new String[15000];
-        //while(paramForRelatedWords<relatedWords2.length){
-            //if (Arrays.asList(relatedWords1).contains(relatedWords2[paramForRelatedWords])) {
-                //matchedWords[matchedNumber] = relatedWords2[paramForRelatedWords];
-            //matchedNumber++;
-            //}
-            //paramForRelatedWords++;
-        //}
-        //System.out.println("--------------------------------------------------");
-        //System.out.println("単語1と単語2のサイトの両方から飛べるリンクの一覧を表示します");
-        //System.out.println("--------------------------------------------------");
-        //System.out.println(Arrays.asList(matchedWords));
-        //if(relatedWords1.length>=relatedWords2.length){
-            //return matchedNumber/relatedWords2.length;
-        //}else{
-            //return matchedNumber / relatedWords1.length;
-        //}
-    //}
+
+    public static ArrayList<Integer> reRelatedWordsSearch(ArrayList<Integer> relatedWordsIndex) {
+        //for relatedWordIndex in relatedWordsIndex; 
+            ArrayList<Integer> reRelatedWords = new ArrayList<Integer>(30000000);   
+            int wordIndexs[] = new int[60000000];
+            int wikiLinks[] = new int[60000000];
+            String[] wikiWords = readWikiWords();
+            try {
+                Scanner scanner = new Scanner(new File("links.txt"));
+                System.out.println("open File, 'links.txt'");
+                int paramForWikiLinks = 0;
+                while (scanner.hasNextLine()) {
+                    // long before = System.nanoTime();
+                    String linkLine = scanner.nextLine();
+                    String[] wordIndexWikiLink = linkLine.split("\t");
+                    wordIndexs[paramForWikiLinks] = Integer.valueOf(wordIndexWikiLink[0]);
+                    wikiLinks[paramForWikiLinks] = Integer.valueOf(wordIndexWikiLink[1]);
+                    paramForWikiLinks++;
+                    // long after = System.nanoTime();
+                    // System.out.println((after - before));
+                    // #wordIndexs=[0,0,0,...]
+                    // #WikiLinks=[284171, 955, 591, ...]
+                }
+
+            } catch (FileNotFoundException e) {
+                System.out.println("can't read File, 'links.txt'");
+            }
+            System.out.println("Finish reading File, 'links.txt'");
+            //ここの中身確認する！
+                for (int paramForReRelatedWords = 0; paramForReRelatedWords < relatedWordsIndex.size(); paramForReRelatedWords++) {
+                    for(int paramForReRelatedWordsSearch=0; paramForReRelatedWordsSearch<wordIndexs.length;paramForReRelatedWordsSearch++){
+                        if(wordIndexs[paramForReRelatedWordsSearch]==relatedWordsIndex.get(paramForReRelatedWords)){
+                            if(!(reRelatedWords.contains(wikiLinks[paramForReRelatedWordsSearch]))){
+                                reRelatedWords.add(wikiLinks[paramForReRelatedWordsSearch]);
+                            }
+                        }else if(wordIndexs[paramForReRelatedWordsSearch]> relatedWordsIndex.get(relatedWordsIndex.size()-1)){
+                            break;
+                        }
+                    }
+            }
+        Collections.sort(reRelatedWords);
+        return   reRelatedWords;
+        
+            
+    }
+
+    public static double reRelatedPerRelated( ArrayList<Integer> relatedWordsIndex, ArrayList<Integer> reRelatedWords ){
+        int matchedCount=0;
+        for(int paramForCalPer=0;paramForCalPer< relatedWordsIndex.size();paramForCalPer++){
+            if(reRelatedWords.contains(relatedWordsIndex.get(paramForCalPer))){
+                matchedCount++;
+            }
+        }
+        return 100*matchedCount/ reRelatedWords.size();
+    }
 
 
 }
 
- //def evaluate_Relationship(relatedWords1,relatedWords2):
-    //matchedNumber=0
-    //matchedWords=[]
-    //if relatedWords1=="null" or relatedWords2=="null":
-        //pass
-
-    //elif len(relatedWords1) <= len(relatedWords2):
-        //for relatedWord1 in relatedWords1:
-            //if relatedWord1 in relatedWords2:
-                //matchedNumber+=1
-                //matchedWords.append(relatedWord1)
-        //print("--------------------------------------------------")
-        //print("単語1と単語2のサイトの両方から飛べるリンクの一覧を表示します")
-        //print("--------------------------------------------------")
-        //print(matchedWords)
-        //return 100*matchedNumber/len(relatedWords1)
-    //else:
-        //for relatedWord2 in relatedWords2:
-            //if relatedWord2 in relatedWords1:
-                //matchedNumber+=1
-                //matchedWords.append(relatedWord2)
-        //print("--------------------------------------------------")
-        //print("単語1と単語2のサイトの両方から飛べるリンクの一覧を表示します")
-        //print("--------------------------------------------------")
-        //print(matchedWords)
-        //return 100*matchedNumber/len(relatedWords2)   
-    
  
